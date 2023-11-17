538be7c5a07f64284fab5777ffd37597c8c3975d
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-11-17 11:01:26 -0300

    Ladrão de Bozó4.0
    não faço ideia se estar  certo, mais na minha cabeça ele deve funcionar bem

--- Ladrão de Bozó - problema 2.c
+++ Ladrão de Bozó - problema 2.c
@@ -18,17 +18,16 @@ int main(){
 	}
 	for(i = 0; i < N; i++){
 		if(vetorcomparacoa[i] != permutacao[i] || checar > 0){
-			printf("\n P %d e D %d", permutacao[i], diferente[checar]);
+			printf("estou aqui\n\n");
 			if(permutacao[i] == ((diferente[checar - 1]) + 1) || checar == 0){
+				printf("estou aqui dentro\n\n");
 				diferente[checar] = permutacao[i];
 				checar += 1;
-				printf("\nestou aqui\n");
 			}
 		}
-		printf("\n P %d e D %d", permutacao[i], diferente[checar]);
 	}
 	for(i = 0; i < checar; i++){
-	   	printf("\n %d\n", diferente[i]);
+	   	printf("\n\n %d\n", diferente[i]);
 	}
 	
 
@@ -39,20 +38,26 @@ int main(){
 			}
 		}
 		for(j = 0; j <= checar; j++){
-			if(diferente[j] < permutacao[i]){
+			if(permutacao[i] > diferente[j]){
 				novovetorcomracoa[i] = diferente[j];
-				for (m = j; m <= checar; m++){
+				for (m = j; m < checar; m++){
 					diferente[m] = diferente[m + 1];
 					checar -= 1;
 				}
 				break;
 			} else if(diferente[0] > permutacao[i]){
 				novovetorcomracoa[i] = permutacao[i];
+				break;
 			}
 		}
 	}
+	if(checar > 0){
+		for(i = 0; i < checar; i++){
+			novovetorcomracoa[((N - checar) - 1) + i] = diferente[i];
+		}
+	}
 	for(i = 0; i < N; i++){
-		printf("%d\n", novovetorcomracoa[i]);
+		printf("\n%d\n", novovetorcomracoa[i]);
 	}
 
 	return 0;
